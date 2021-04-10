004296d520038909914fc8791244e9b12e493228
msharma-tw <monika.sharma@thoughtworks.com>
2021-04-11 02:04:23 +0530

    ms | added options for ramping users

--- tests/simulations.spec.js
+++ tests/simulations.spec.js
@@ -1,10 +1,22 @@
 import http from 'k6/http';
 import { check, group } from 'k6';
 
+export let options = {
+  vus: 1,
+  duration: '30s',
+};
+
+// ramping up users
+// export let options = {
+//   stages: [
+//     { duration: '30s', target: 20 },
+//     { duration: '1m30s', target: 10 },
+//     { duration: '20s', target: 0 },
+//   ],
+// };
+
 export default function () {
-
   group('generate login token', function () {
-
     var url = 'https://test-api.k6.io/auth/token/login/';
     var payload = JSON.stringify({
       username: `${__ENV.USERNAME}`,
@@ -21,5 +33,4 @@ export default function () {
       'status is 200': (r) => r.status == 200,
     });
   });
-
 }
